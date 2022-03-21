2e46a44f359a42097d3557339f7ab0a45dcce63b
maksimo-vvd <maksim.o@vevidi.com>
2022-03-21 17:14:39 +0200

    Props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,48 +1,30 @@
 import React from 'react';
-
-import Instructions from '../Instructions/Instructions';
-
+import data from './data';
+import AnimalCard from '../AnimalCard/AnimalCard';
 import './App.css';
 
-const displayEmojiName = event => alert(event.target.id);
-const emojis = [
-  {
-    emoji: "😀",
-    name: "grinning face"
-  },
-  {
-    emoji: "✎",
-    name: "party popper"
-  },
-  {
-    emoji: "☕",
-    name: "woman dancing"
-  }
-];
-const displayAction = false;
+function showAdditional(additional) {
+  const alertInformation = Object.entries(additional)
+    .map(information => `${information[0]}: ${information[1]}`)
+    .join('\n');
+  alert(alertInformation)
+};
 
 function App() {
-  const greeting = "greeting";
-  return(
-    <div className="container">
-      <h1 id={greeting}>Hello, World</h1>
-      {!displayAction && <p>I am writing JSX</p>}
-
-      <Instructions />
-
-      <ul>
-        {
-          emojis.map(emoji => (
-            <li key={emoji.name}>
-              <button
-                onClick={displayEmojiName}
-              >
-                <span role="img" aria-label={emoji.name} id={emoji.name}>{emoji.emoji}</span>
-              </button>
-            </li>
-          ))
-        }
-      </ul>
+  return (
+    <div className="wrapper">
+      <h1>Animals</h1>
+      {data.map(animal => (
+        <AnimalCard
+          additional={animal.additional}
+          diet={animal.diet}
+          key={animal.name}
+          name={animal.name}
+          scientificName={animal.scientificName}
+          size={animal.size}
+          showAdditional={showAdditional}
+        />
+      ))}
     </div>
   )
 }
