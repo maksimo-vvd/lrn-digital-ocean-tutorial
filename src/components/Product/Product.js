5992de067dbb434a07f0b992d3e54032b1d5f575
maksimo-vvd <maksim.o@vevidi.com>
2022-03-30 18:23:38 +0300

    How To Manage State with Hooks on React Components

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, {useReducer, useState} from 'react';
 import './Product.css';
 
+// Product items
 const products = [
   {
     emoji: '☕',
@@ -18,76 +19,81 @@ const products = [
     price: 4,
   },
 ]
+// Options for format currencies according browser settings
+const currencyOptions = {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 4,
+}
 
-export default class Product extends Component {
-  state = {
-    cart: [],
-    // total: 0,
-  }
-  currentOptions = {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  }
+// Total items coast
+function getTotal(cart) {
+  // Counting the items price
+  const total = cart.reduce((totalCost, item) => totalCost + item.price, 0)
+  // Format price to string to return
+  return total.toLocaleString(undefined, currencyOptions)
+}
 
-  add = (product) => {
-    this.setState(state => {
-      return {
-        cart: [...state.cart, product],
-      }
-    })
-  }
-
-  getTotal = () => {
-    const total = this.state.cart.reduce((totalCost, item) => totalCost + item.price, 0)
-    // return this.state.total.toLocaleString(undefined, this.currentOptions)
-    return total.toLocaleString(undefined, this.currentOptions)
-  }
-
-  // remove = (product) => {
-  //   this.setState( state => {
-  //     const cart = [...state.cart]
-  //     cart.splice(cart.indexOf(product.name))
-  //     return ({
-  //       cart,
-  //       total: state.total - product.price,
-  //     })
-  //   })
-  // }
-  remove = (product) => {
-    this.setState( state => {
-      const cart = [...state.cart]
-      const productIndex = cart.findIndex(p => p.name === product.name)
+// Reducer hook callback. Action by type
+function cartReducer(state, action) {
+  switch (action.type) {
+    case 'add':
+      return [...state, action.product]
+    case 'remove':
+      // Find index of item in state by name
+      const productIndex = state.findIndex(item => item.name === action.product.name)
+      // Check is item present
       if (productIndex < 0) {
-        return
+        // If item not exist return not modified state
+        return state
       }
-      cart.splice(productIndex, 1)
-      return ({cart})
-    })
+      // If item exist in state.
+      // Create the state clone
+      const update =  [...state]
+      //  update.splice(update.findIndex(()=>action.name), 1)
+      //  update.splice(update.indexOf(action.name), 1);
+      // Cut the target item from the state clone
+      update.splice(productIndex, 1)
+      // Return updated state
+      return update
+    default:
+      return state
+  }
+}
+
+export default function Product() {
+  // Reducer hook.
+  const [cart, setCart ] = useReducer(cartReducer,[])
+
+  function add(product) {
+    // The function for useReducer's callback
+    // The object with item for operation and action type
+    setCart({ product, type: 'add' })
   }
 
-  render() {
-    return(
-      <div className="wrapper">
-        <div>
-          <b>Shopping Cart:</b> <u>{this.state.cart.length} total items</u>.
-        </div>
+  function remove(product) {
+    setCart({ product, type: 'remove' })
+  }
 
-        {/*<div>Total: {this.state.total}</div>*/}
-        <div><b>Total:</b> <u>{this.getTotal()}</u></div>
-
-        <section className="section">
-          {products.map(product => (
-            <div key={product.name} className="product-holder">
-              <div className="product">
-                <span role="img" aria-label={product.name}>{product.emoji}</span>
-              </div>
-
-              <button onClick={()=> this.add(product)}>Add</button>
-              <button onClick={() => this.remove(product)}>Remove</button>
-            </div>
-          ))}
-        </section>
+  return(
+    <div className="wrapper">
+      <div>
+        <b>Shopping Cart:</b> <u>{cart.length} total items</u>.
       </div>
-    )
-  }
+
+      <div><b>Total:</b> <u>{getTotal(cart)}</u></div>
+
+      <section className="section">
+        {products.map(product => (
+          <div key={product.name} className="product-holder">
+            <div className="product">
+              <span role="img" aria-label={product.name}>{product.emoji}</span>
+            </div>
+
+            <button onClick={() => add(product)}>Add</button>
+            <button onClick={() => remove(product)}>Remove</button>
+          </div>
+        ))}
+      </section>
+    </div>
+  )
 }
